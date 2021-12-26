9e4e7b10389487d08d75928920d8b0e59a93783a
KervinDd <kervindenis16@gmail.com>
2021-12-26 12:05:19 +0100

    f

---
+++ src/main/java/Level.java
@@ -0,0 +1,16 @@
+public class Level extends AbstractLevel{
+    @Override
+    public String toString() {
+        return null;
+    }
+
+    @Override
+    LevelComponent[][] getComponents() {
+        return new LevelComponent[0][];
+    }
+
+    @Override
+    int getSize() {
+        return 0;
+    }
+}
